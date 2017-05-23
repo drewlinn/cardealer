ed4b6624a92728c59ad027b16a3454aeaef53861
Andrew Dalton and Ethan Luts <andrew+ethan@>
2017-05-23 11:03:29 -0700

    incorporate all progress on dealership app from monday.

---
+++ Modules/HomeModule.cs
@@ -0,0 +1,15 @@
+using Nancy;
+using Car.Objects;
+using System.Collections.Generic;
+
+namespace Car
+{
+  public class HomeModule : NancyModule
+  {
+    public HomeModule()
+    {
+      
+
+    }
+  }
+}
