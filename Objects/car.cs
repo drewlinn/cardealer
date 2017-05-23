06a86c134517320dec64ccbb55cb5477cad9465d
Andrew Dalton and Ethan Luts <andrew+ethan@>
2017-05-23 15:08:40 -0700

    complete most of our page elements and begin working on functionality.

--- Objects/car.cs
+++ Objects/car.cs
@@ -1,64 +1,54 @@
 using System;
 using System.Collections.Generic;
 
-class Car
+namespace CarDealer.Objects
 {
-  public string MakeModel;
-  public int Price;
-  public int Miles;
-  public bool WorthBuying(int maxPrice, int maxMiles)
+
+  class Car
   {
-    return Price < (maxPrice + 100);
-  }
-}
+    private string _makeModel;
+    private string _price;
+    private string _miles;
+    private static List<Car> _carDealer = new List<Car> {};
 
-public class Program
-{
-  public static void Main()
-  {
-    Car porsche = new Car();
-    porsche.MakeModel = "2014 Porsche 911";
-    porsche.Price = 114991;
-    porsche.Miles = 7864;
-
-    Car ford = new Car();
-    ford.MakeModel = "2011 Ford F450";
-    ford.Price = 55995;
-    ford.Miles = 14241;
-
-    Car lexus = new Car();
-    lexus.MakeModel = "2013 Lexus RX 350";
-    lexus.Price = 44700;
-    lexus.Miles = 20000;
-
-    Car mercedes = new Car();
-    mercedes.MakeModel = "Mercedes Benz CLS550";
-    mercedes.Price = 39900;
-    mercedes.Miles = 37979;
-
-    List<Car> Cars = new List<Car>() { porsche, ford, lexus, mercedes };
-
-    Console.WriteLine("Enter maximum price: ");
-    string stringMaxPrice = Console.ReadLine();
-    int maxPrice = int.Parse(stringMaxPrice);
-
-    Console.WriteLine("Enter maximum miles: ");
-    string stringMaxMiles = Console.ReadLine();
-    int maxMiles = int.Parse(stringMaxMiles);
-
-    List<Car> CarsMatchingSearch = new List<Car>(0);
-
-    foreach (Car automobile in Cars)
+    public Car(string makeModel, string price, string miles)
     {
-      if (automobile.WorthBuying(maxPrice, maxMiles))
-      {
-        CarsMatchingSearch.Add(automobile);
-      }
+      _makeModel = makeModel;
+      _price = price;
+      _miles = miles;
     }
 
-    foreach(Car automobile in CarsMatchingSearch)
+    public string GetMakeModel()
     {
-      Console.WriteLine(automobile.MakeModel);
+      return _makeModel;
+    }
+    public void SetMakeModel(string newMakeModel)
+    {
+      _makeModel = newMakeModel;
+    }
+    public string GetPrice()
+    {
+      return _price;
+    }
+    public void SetPrice(string newPrice)
+    {
+      _price = newPrice;
+    }
+    public string GetMiles()
+    {
+      return _miles;
+    }
+    public void SetMiles(string newMiles)
+    {
+      _miles = newMiles;
+    }
+    public static List<Car> GetAll()
+    {
+      return _carDealer;
+    }
+    public void Save()
+    {
+      _carDealer.Add(this);
     }
   }
 }
